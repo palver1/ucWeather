0e69a917acafbbf07e60cfe1849132abd9abefa5
palvert <Pavelver39@gmail.com>
2025-12-24 21:57:24 +0700

    make http request func

--- src/ucWeather.c
+++ src/ucWeather.c
@@ -1,19 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "../libs/curl/include/curl/curl.h"
 
+#include "../libs/curl/include/curl/curl.h"
 #include "../libs/lib_json_parser/json_parser.h"
 
 char API[50];
 char URL[200];
 char *history;
 
-int main(void)
-{
+size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata);
+
+int main(void) {   
     /* MAKE URL ============================================================================== */
     FILE *file_api = fopen("out/api.key", "r"); //TODO: fix relative path problem
-    if (file_api == NULL) { perror("ERROR: Unable to load api.key file"); exit(EXIT_FAILURE); }
-
+    if (file_api == NULL) { perror("ERROR: Unable to load the api.key file"); exit(EXIT_FAILURE); }
+    
     fread(API, 50, 1, file_api);
     /* Remove nl-char */
     char *c = API;
@@ -24,17 +25,16 @@ int main(void)
 
     /* SEND HTTP REQUEST ============================================================================== */
     CURL *curl;
-
     CURLcode res = curl_global_init(CURL_GLOBAL_ALL);
     if(res) {
+        perror("curl failed");
         return (int)res;
     }
 
     curl = curl_easy_init();
     if(curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, URL);
-        /* cache the CA cert bundle in memory for a week */
-        // curl_easy_setopt(curl, CURLOPT_CA_CACHE_TIMEOUT, 604800L);
+        curl_easy_setopt(curl, CURLOPT_URL, URL);                      // set up URL address
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback); // set up callback function
 
         /* Perform the request, res gets the return code */
         res = curl_easy_perform(curl);
@@ -48,9 +48,16 @@ int main(void)
 
     /* SAVE DATA TO THE HISTORY ============================================================================== */
     puts("\n");
-    printf("ANSWER: %s\n", (char*)curl);
 
     curl_global_cleanup();
 
     return (int)res;
+}
+
+
+size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata) {
+    puts("HERE'S THE SAVED DATA"); // DEBUG
+    printf("%s", JP_get_value(ptr, "country")); // DEBUG
+
+    return nmemb;
 }
